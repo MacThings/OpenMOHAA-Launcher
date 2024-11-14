6436701723db4d0b927fd2d6667a39f7a3d21149
MacThings <info@sl-soft.de>
2024-11-14 15:24:40 +0100

    Initial

---
+++ OpenMOHAA Launcher/ViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  OpenMOHAA GUI
+//
+//  Created by Prof. Dr. Luigi on 13.11.24.
+//
+
+import Cocoa
+import Foundation
+import AppKit
+
+class ViewController: NSViewController {
+    
+    @IBOutlet var StartButton: NSButton!
+    @IBOutlet weak var Warning: NSTextField!
+    
+    @IBOutlet weak var resolutionPopUpButton: NSPopUpButton!
+    
+    let scriptPath = Bundle.main.path(forResource: "/script/script", ofType: "command")!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.syncShellExec(path: self.scriptPath, args: ["init"])
+        
+        let gametype = UserDefaults.standard.string(forKey: "GameType")
+        if gametype == nil{
+            UserDefaults.standard.set("0", forKey: "GameType")
+        }
+        
+        self.syncShellExec(path: self.scriptPath, args: ["validate"])
+        
+        checkValidation()
+        
+        populateResolutionMenu()
+        
+        // Prüfe, ob bereits eine gespeicherte Auflösung existiert
+                if let savedResolution = UserDefaults.standard.string(forKey: "Resolution") {
+                    // Wenn ja, setze diese als Vorauswahl
+                    resolutionPopUpButton.selectItem(withTitle: savedResolution)
+                } else if let firstResolution = resolutionPopUpButton.itemTitles.first {
+                    // Wenn keine gespeicherte Auflösung existiert, setze die erste Auflösung als Standard
+                    resolutionPopUpButton.selectItem(withTitle: firstResolution)
+                    UserDefaults.standard.set(firstResolution, forKey: "Resolution")
+                }
+
+        }
+        
+        // Do any additional setup after loading the view
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    func populateResolutionMenu() {
+            resolutionPopUpButton.removeAllItems()
+            
+            for screen in NSScreen.screens {
+                if let screenID = screen.deviceDescription[NSDeviceDescriptionKey("NSScreenNumber")] as? CGDirectDisplayID {
+                    
+                    if let modes = CGDisplayCopyAllDisplayModes(screenID, nil) as? [CGDisplayMode] {
+                        for mode in modes {
+                            let width = Int(mode.width)
+                            let height = Int(mode.height)
+                            let resolutionString = "\(width) x \(height)"
+                            
+                            if resolutionPopUpButton.itemTitles.contains(resolutionString) == false {
+                                resolutionPopUpButton.addItem(withTitle: resolutionString)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        @IBAction func resolutionChanged(_ sender: NSPopUpButton) {
+            if let selectedResolution = sender.selectedItem?.title {
+                UserDefaults.standard.set(selectedResolution, forKey: "Resolution")
+            }
+        }
+    
+    
+    @IBAction func OpenPath(_ sender: Any) {
+        let userPath = NSHomeDirectory() + "/Library/Application Support/openmohaa"
+        let url = URL(fileURLWithPath: userPath)
+
+        if FileManager.default.fileExists(atPath: userPath) {
+            NSWorkspace.shared.activateFileViewerSelecting([url])
+        } else {
+            print("Dateipfad existiert nicht: \(userPath)")
+        }
+    }
+
+    
+    @IBAction func ValidateGame(_ sender: Any) {
+        //DispatchQueue.main.async {
+            self.syncShellExec(path: self.scriptPath, args: ["validate"])
+        //}
+        
+        checkValidation()
+    }
+    
+    
+    @IBAction func StartGame(_ sender: Any) {
+        
+        checkValidation()
+        
+        self.syncShellExec(path: self.scriptPath, args: ["validate"])
+        
+        let gamevalid = UserDefaults.standard.string(forKey: "GameValid")
+        if gamevalid == "1" {
+            DispatchQueue.main.async {
+                self.syncShellExec(path: self.scriptPath, args: ["start"])
+            }
+        } else {
+            self.Warning.isHidden = false
+            self.StartButton.isEnabled = false
+        }
+    }
+    
+    func checkValidation() {
+        let gamevalid = UserDefaults.standard.string(forKey: "GameValid")
+        if gamevalid == "1" {
+            self.StartButton.isEnabled = true
+            self.Warning.isHidden = true
+        } else {
+            self.StartButton.isEnabled = false
+            self.Warning.isHidden = false
+            return
+        }
+    }
+    
+    func syncShellExec(path: String, args: [String] = []) {
+        let process            = Process()
+        process.launchPath     = "/bin/bash"
+        process.arguments      = [path] + args
+        let outputPipe         = Pipe()
+        process.standardOutput = outputPipe
+        process.launch() // Start process
+        process.waitUntilExit() // Wait for process to terminate.
+    }
+    
+
+}
+
---
+++ OpenMOHAA Launcher/script/script.command
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+#
+
+ScriptHome=$(echo $HOME)
+MY_PATH="`dirname \"$0\"`"
+cd "$MY_PATH"
+
+function _helpDefaultRead()
+{
+    VAL=$1
+    
+    if [ ! -z "$VAL" ]; then
+    defaults read "${ScriptHome}/Library/Preferences/sl-soft.openmohaa-launcher.plist" "$VAL"
+    fi
+}
+
+function _helpDefaultWrite()
+{
+    VAL=$1
+    local VAL1=$2
+    
+    if [ ! -z "$VAL" ] || [ ! -z "$VAL1" ]; then
+    defaults write "${ScriptHome}/Library/Preferences/sl-soft.openmohaa-launcher.plist" "$VAL" "$VAL1"
+    fi
+}
+
+function validate() {
+
+    gametype=$( _helpDefaultRead "GameType" )
+
+    if [[ "$gametype" = "0" ]]; then
+        if [ -d "/Users/$USER/Library/Application Support/openmohaa/main" ]; then
+            _helpDefaultWrite "GameValid" "1"
+        else
+            _helpDefaultWrite "GameValid" "0"
+        fi
+    fi
+        
+    if [[ "$gametype" = "1" ]]; then
+        if [ -d "/Users/$USER/Library/Application Support/openmohaa/mainta" ] && [ -d "/Users/$USER/Library/Application Support/openmohaa/main" ]; then
+            _helpDefaultWrite "GameValid" "1"
+        else
+            _helpDefaultWrite "GameValid" "0"
+        fi
+    fi
+    
+    if [[ "$gametype" = "2" ]]; then
+        if [ -d "/Users/$USER/Library/Application Support/openmohaa/maintt" ] && [ -d "/Users/$USER/Library/Application Support/openmohaa/main" ]; then
+            _helpDefaultWrite "GameValid" "1"
+        else
+            _helpDefaultWrite "GameValid" "0"
+        fi
+    fi
+
+}
+
+function init() {
+
+if [ ! -d "/Users/$USER/Library/Application Support/openmohaa" ]; then
+    mkdir "/Users/$USER/Library/Application Support/openmohaa"
+fi
+
+}
+function start() {
+
+    screen_width=$( _helpDefaultRead "Resolution" | sed 's/x.*//g' |xargs )
+    screen_height=$( _helpDefaultRead "Resolution" | sed 's/.*x//g' |xargs )
+    gametype=$( _helpDefaultRead "GameType" )
+    gamevalid=$( _helpDefaultRead "GameValid" )
+
+    cd ../bin
+
+    if [[ "$gametype" = "0" ]] && [[ "$gamevalid" = "1" ]]; then
+        sed -i '' '/r_mode/d' "/Users/$USER/Library/Application Support/openmohaa/main/configs/omconfig.cfg"
+        sed -i '' '/r_customwidth/d' "/Users/$USER/Library/Application Support/openmohaa/main/configs/omconfig.cfg"
+        sed -i '' '/r_customheight/d' "/Users/$USER/Library/Application Support/openmohaa/main/configs/omconfig.cfg"
+        echo 'seta r_mode "-1"' >> "/Users/$USER/Library/Application Support/openmohaa/main/configs/omconfig.cfg"
+        echo "seta r_customwidth \"$screen_width\"" >> "/Users/$USER/Library/Application Support/openmohaa/main/configs/omconfig.cfg"
+        echo "seta r_customheight \"$screen_height\"" >> "/Users/$USER/Library/Application Support/openmohaa/main/configs/omconfig.cfg"
+        ./openmohaa +set com_target_game 0
+    elif [[ "$gametype" = "1" ]] && [[ "$gamevalid" = "1" ]]; then
+        sed -i '' '/r_mode/d' "/Users/$USER/Library/Application Support/openmohaa/mainta/configs/omconfig.cfg"
+        sed -i '' '/r_customwidth/d' "/Users/$USER/Library/Application Support/openmohaa/mainta/configs/omconfig.cfg"
+        sed -i '' '/r_customheight/d' "/Users/$USER/Library/Application Support/openmohaa/mainta/configs/omconfig.cfg"
+        echo 'seta r_mode "-1"' >> "/Users/$USER/Library/Application Support/openmohaa/mainta/configs/omconfig.cfg"
+        echo "seta r_customwidth \"$screen_width\"" >> "/Users/$USER/Library/Application Support/openmohaa/mainta/configs/omconfig.cfg"
+        echo "seta r_customheight \"$screen_height\"" >> "/Users/$USER/Library/Application Support/openmohaa/mainta/configs/omconfig.cfg"
+        ./openmohaa +set com_target_game 1
+    elif [[ "$gametype" = "2" ]] && [[ "$gamevalid" = "1" ]]; then
+        sed -i '' '/r_mode/d' "/Users/$USER/Library/Application Support/openmohaa/maintt/configs/omconfig.cfg"
+        sed -i '' '/r_customwidth/d' "/Users/$USER/Library/Application Support/openmohaa/maintt/configs/omconfig.cfg"
+        sed -i '' '/r_customheight/d' "/Users/$USER/Library/Application Support/openmohaa/maintt/configs/omconfig.cfg"
+        echo 'seta r_mode "-1"' >> "/Users/$USER/Library/Application Support/openmohaa/maintt/configs/omconfig.cfg"
+        echo "seta r_customwidth \"$screen_width\"" >> "/Users/$USER/Library/Application Support/openmohaa/maintt/configs/omconfig.cfg"
+        echo "seta r_customheight \"$screen_height\"" >> "/Users/$USER/Library/Application Support/openmohaa/maintt/configs/omconfig.cfg"
+        ./openmohaa +set com_target_game 2
+    fi
+    
+}
+
+$1
