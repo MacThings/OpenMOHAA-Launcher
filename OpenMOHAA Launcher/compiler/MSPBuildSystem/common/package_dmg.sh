dad058d7583ac17b596f8b745d619f1df3094c9a
MacThings <info@sl-soft.de>
2024-11-22 10:25:51 +0100

    Test

--- OpenMOHAA Launcher/compiler/MSPBuildSystem/common/package_dmg.sh
+++ OpenMOHAA Launcher/compiler/MSPBuildSystem/common/package_dmg.sh
@@ -2,21 +2,13 @@
 mkdir -p ${BUILT_PRODUCTS_DIR}/source_folder
 mv "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}" ${BUILT_PRODUCTS_DIR}/source_folder
 
-
 #move app bundle back to parent folder
 echo mv "${BUILT_PRODUCTS_DIR}/source_folder/${WRAPPER_NAME}" ${BUILT_PRODUCTS_DIR}
 mv "${BUILT_PRODUCTS_DIR}/source_folder/${WRAPPER_NAME}" ${BUILT_PRODUCTS_DIR}
 rm -rd ${BUILT_PRODUCTS_DIR}/source_folder
 
-if [ "$1" != "skipdelete" ]; then
-  if [ -d "../MSPBuildSystem/${PROJECT_NAME}/release-${APP_VERSION}${ARCH_FOLDER}_${DATE_TIMESTAMP}" ]; then
-    rm -rf "../MSPBuildSystem/${PROJECT_NAME}/release-${APP_VERSION}${ARCH_FOLDER}_${DATE_TIMESTAMP}" || exit 1;
-  fi
-  mkdir -p "../MSPBuildSystem/${PROJECT_NAME}/release-${APP_VERSION}${ARCH_FOLDER}_${DATE_TIMESTAMP}";
-fi
-
 rm -rf ${BUILT_PRODUCTS_DIR}/openmohaa.app/Contents/MacOS/libs-*
-cp -a ${BUILT_PRODUCTS_DIR}/openmohaa.app/Contents/MacOS/* "../MSPBuildSystem/${PROJECT_NAME}/release-${APP_VERSION}${ARCH_FOLDER}_${DATE_TIMESTAMP}"
+cp -a ${BUILT_PRODUCTS_DIR}/openmohaa.app/Contents/MacOS/* "../../bin"
 
 if [ "$1" != "skipcleanup" ] && [ "$2" != "skipcleanup" ]; then
     echo "Cleaning up"
